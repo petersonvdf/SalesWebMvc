cfcd3b6c348a6804a27233d9a5ec8c437b858100
Peterson Freitas <petersonvdf@gmail.com>
2020-12-18 12:20:45 -0300

    Other entities and second migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -10,6 +10,8 @@ namespace SalesWebMvc.Data
         {
         }
 
+        public DbSet<Seller> Seller { get; set; }
         public DbSet<Department> Department { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
